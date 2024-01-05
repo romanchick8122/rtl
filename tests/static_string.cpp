a68715559a84edcbdc4135334657456191308c62
Raman Svistunou <roma-svistunov@tut.by>
2024-01-05 17:28:04 +0300

    Docs + theme fixes + updated test to reflect examples.

--- tests/static_string.cpp
+++ tests/static_string.cpp
@@ -1,8 +1,8 @@
 #include <static_string.hpp>
 
-template<rtl::static_string> struct literal_template{};
-static_assert(requires {
-    literal_template<"string">{};
-});
+template<rtl::static_string value>
+constexpr static char first_letter = value.data[0];
+
+static_assert(first_letter<"James"> == 'J');
 int main(int, char**) {
 }
